0c59232b1bee5a7075603f24168746738123bce3
Kirill <dronfaraonx@gmail.com>
2024-07-13 21:24:36 +0700

    score branch changes

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const fs = require("fs");
+// const fs = require("fs");
 
-const arrWordList = fs.readFileSync(__dirname + "./word-list.txt", "utf8").split("\n");
+// const arrWordList = fs.readFileSync("./word-list.txt", "utf8").split("\n");
 
-function seurch(arr, text) {
-  let result = false;
-  for (let i = 0; i < arr.length; i += 1) {
-    if (arr[i] == text) {
-        return true;
-    }
-  }
-  return result;
-}
-// const text = 'abirritative'
-console.log(seurch(arrWordList, text));
+// function seurch(arr, text) {
+//   let result = false;
+//   for (let i = 0; i < arr.length; i += 1) {
+//     if (arr[i] == text) {
+//         return true;
+//     }
+//   }
+//   return result;
+// }
+// // const text = 'abirritative'
+// console.log(seurch(arrWordList, text));
 
 
 
